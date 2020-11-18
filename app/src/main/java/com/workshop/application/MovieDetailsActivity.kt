17887ca6cbe3f22a8707d7e299f40b101708f98b
dsokol_ok <sdashas107@gmail.com>
2020-11-18 14:15:46 +0300

    add activity

--- app/src/main/java/com/workshop/application/MovieDetailsActivity.kt
+++ app/src/main/java/com/workshop/application/MovieDetailsActivity.kt
@@ -1,14 +1,11 @@
 package com.workshop.application
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this, MovieDetailsActivity::class.java))
+        setContentView(R.layout.activity_movie_details)
     }
 }
